83f28f55b0724a8b61a57be90f602a24c6a96bf2
MichaelC27 <dbpanama2796@gmail.com>
2024-07-08 17:47:03 -0500

    Se implemento Fragment

---
+++ app/src/main/java/com/laabbb/newsnow/Clases/noticias.java
@@ -0,0 +1,153 @@
+package com.laabbb.newsnow.Clases;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
+import androidx.lifecycle.Lifecycle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.laabbb.newsnow.Adaptadores.NoticiaAdaptador;
+import com.laabbb.newsnow.R;
+import com.laabbb.newsnow.Clases.Noticia;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class noticias extends Fragment implements MenuProvider {
+
+    private RecyclerView recNoticia;
+    private RequestQueue requestQueue;
+    private NoticiaAdaptador adaptador;
+    private ArrayList<Noticia> noticias = new ArrayList<>();
+    private String url = "https://p3.qr4me.net/";
+    private Toolbar toolbar;
+
+    public noticias() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_noticias, container, false);
+
+        // Inicializar Toolbar
+        toolbar = view.findViewById(R.id.toolbar2);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        // Inicializar RecyclerView y adaptador
+        recNoticia = view.findViewById(R.id.recNoticia);
+        adaptador = new NoticiaAdaptador(getActivity(), noticias);
+        recNoticia.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recNoticia.setAdapter(adaptador);
+
+        // Obtener noticias del servidor
+        fetchNoticias();
+
+        // Añadir el MenuProvider al MenuHost
+        MenuHost menuHost = requireActivity();
+        menuHost.addMenuProvider(this, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.menuinicio, menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.btn_vizualizacion) {
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
+            View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet_dialog, null);
+            bottomSheetDialog.setContentView(bottomSheetView);
+            // Configura los listeners para las opciones del BottomSheet
+            bottomSheetView.findViewById(R.id.btn_lista).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adaptador = new NoticiaAdaptador(getActivity(), noticias);
+                    recNoticia.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+                    recNoticia.setAdapter(adaptador);
+                    bottomSheetDialog.dismiss();
+                }
+            });
+
+            bottomSheetView.findViewById(R.id.btn_tabla).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adaptador = new NoticiaAdaptador(getActivity(), noticias);
+                    recNoticia.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    recNoticia.setAdapter(adaptador);
+                    bottomSheetDialog.dismiss();
+                }
+            });
+            bottomSheetDialog.show();
+            return true;
+        }
+        return false;
+    }
+
+    private void fetchNoticias() {
+        requestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                parseNoticias(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                // Manejar el error de la petición aquí
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private void parseNoticias(String response) {
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                noticias.add(new Noticia(
+                        jsonObject.getString("id"),
+                        jsonObject.getString("titulo"),
+                        jsonObject.getString("autor"),
+                        "",
+                        0,
+                        jsonObject.getString("imagen"),
+                        jsonObject.getString("icono")
+                ));
+            }
+            adaptador.notifyDataSetChanged(); // Notificar al adaptador de los cambios en los datos
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- app/src/main/java/com/laabbb/newsnow/MainActivity.java
+++ app/src/main/java/com/laabbb/newsnow/MainActivity.java
@@ -3,50 +3,27 @@ package com.laabbb.newsnow;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.laabbb.newsnow.Adaptadores.NoticiaAdaptador;
-import com.laabbb.newsnow.Clases.Noticia;
+import com.laabbb.newsnow.Clases.noticias;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recNoticia;
-    RequestQueue requestQueue;
-    StringRequest stringRequest;
-    NoticiaAdaptador adaptador;
-    Toolbar toolbar;
-    ImageButton btn_lista, btn_tabla;
-    BottomNavigationView bottomNavigationView;
-    String url = "https://p3.qr4me.net/";
-    ArrayList<Noticia> noticias = new ArrayList<>();
-    Map<Integer, Runnable> menuActions;
+
+    private BottomNavigationView bottomNavigationView;
+    private Map<Integer, Runnable> menuActions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,16 +37,13 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Inicialización del Toolbar
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         // Inicialización del BottomNavigationView y configuración del listener
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         // Inicialización del mapa de acciones
         menuActions = new HashMap<>();
-        menuActions.put(R.id.btn_noticias, () -> Toast.makeText(MainActivity.this, "Noticias seleccionadas", Toast.LENGTH_SHORT).show());
+        menuActions.put(R.id.btn_noticias, () -> loadFragment(new noticias()));
         menuActions.put(R.id.btn_favoritos, () -> Toast.makeText(MainActivity.this, "Favoritos seleccionados", Toast.LENGTH_SHORT).show());
         menuActions.put(R.id.btn_usuario, () -> Toast.makeText(MainActivity.this, "Usuario seleccionado", Toast.LENGTH_SHORT).show());
 
@@ -82,92 +56,17 @@ public class MainActivity extends AppCompatActivity {
             return false;
         });
 
-        // Inicializar RecyclerView y adaptador
-        initializeRecyclerView();
-
-        // Obtener noticias del servidor
-        fetchNoticias();
-    }
-
-    // Método para inicializar el RecyclerView y su adaptador
-    private void initializeRecyclerView() {
-        recNoticia = findViewById(R.id.recNoticia);
-        adaptador = new NoticiaAdaptador(this, noticias);
-        recNoticia.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        recNoticia.setAdapter(adaptador);
-    }
-
-    // Método para obtener noticias desde el servidor
-    private void fetchNoticias() {
-        requestQueue = Volley.newRequestQueue(this);
-        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                parseNoticias(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                // Manejar el error de la petición aquí
-            }
-        });
-        requestQueue.add(stringRequest);
-    }
-
-    // Método para parsear el JSON de las noticias
-    private void parseNoticias(String response) {
-        try {
-            JSONArray jsonArray = new JSONArray(response);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                noticias.add(new Noticia(
-                        jsonObject.getString("id"),
-                        jsonObject.getString("titulo"),
-                        jsonObject.getString("autor"),
-                        "",
-                        0,
-                        jsonObject.getString("imagen"),
-                        jsonObject.getString("icono")
-                ));
-            }
-            adaptador.notifyDataSetChanged(); // Notificar al adaptador de los cambios en los datos
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
+        // Cargar fragmento inicial
+        if (savedInstanceState == null) {
+            loadFragment(new noticias());
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menuinicio, menu);
-        return super.onCreateOptionsMenu(menu);
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.btn_vizualizacion) {
-            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
-            View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet_dialog, null);
-            bottomSheetDialog.setContentView(bottomSheetView);
-            // Configura los listeners para las opciones del BottomSheet
-            bottomSheetView.findViewById(R.id.btn_lista).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    adaptador = new NoticiaAdaptador(MainActivity.this, noticias);
-                    recNoticia.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
-                    recNoticia.setAdapter(adaptador);
-                }
-            });
-
-            bottomSheetView.findViewById(R.id.btn_tabla).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    adaptador = new NoticiaAdaptador(MainActivity.this, noticias);
-                    recNoticia.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                    recNoticia.setAdapter(adaptador);
-                }
-            });
-            bottomSheetDialog.show();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
